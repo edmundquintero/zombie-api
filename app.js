f904724b9d4468f84c0a0ac00c651ef212237563
Edmund Quintero <equintero@tradeking.com>
2017-02-15 18:12:24 -0500

    Builds out default game items table and routs

--- app.js
+++ app.js
@@ -1,70 +1,56 @@
-const consoel = console;
-
 const express = require('express');
 const app = express();
-var fs = require('fs');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./test.db');
 
-db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='lorem'", (error, table) => {
-    console.log(table);
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='items'", (error, table) => {
+
     if ( !table ) {
 
         db.serialize( () => {
-          db.run('CREATE TABLE lorem (info TEXT)');
-          const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+            db.run('CREATE TABLE items (name TEXT, desc TEXT, weight INT, dmg INT, healAmt INT)');
+            const stmt = db.prepare('INSERT INTO items VALUES (?, ?, ?, ?, ?)');
 
-          for (var i = 0; i < 100; i++) {
-            stmt.run('Ipsum ' + i);
-          }
+            stmt.run('Apple', 'Red', 0.5, 1, 10);
+            stmt.run('Watermellon', 'Green', 2.5, 1, 20);
+            stmt.run('Ninja Star', 'Silver', 0.2, 5, 0);
+            stmt.run('Cricket Bat', 'Brown', 1.5, 3, 0);
 
-          stmt.finalize();
+            stmt.finalize();
 
-          db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-            console.log('Loaded: ', row.id + ': ' + row.info);
-          })
+            db.each('SELECT rowid AS id, name FROM items', (err, row) => {
+                console.log('Loaded: ', row.id + ': ' + row.name);
+            })
         })
     }
 });
 
 
     // db.close();
-
-
 app.get('/', (req, res) => {
     res.send( '<h1>Future home of the Zombie API! </h1>' +
         '<ul>' +
-        '<li> <a href="/test">All Test Data</a> </li>' +
-        '<li> <a href="/test/3">Single Test Data with Id: 3</a> </li>' +
+        '<li> <a href="/items">All Test Data</a> </li>' +
+        '<li> <a href="/items/3">Single Test Data with Id: 3</a> </li>' +
         '</ul>' );
 });
 
-app.get('/test', (req, res) => {
-    const returnData = [];
-    db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-        returnData.push({
-            id: row.id,
-            info: row.info
-        });
-    }, () => {
-        res.send( JSON.stringify( returnData ) );
+app.get('/items', (req, res) => {
+    db.all('SELECT rowid AS id, name, desc, weight, dmg, healAmt FROM items', (err, row) => {
+        res.send( JSON.stringify( row ) );
     });
 });
 
-app.get('/test/:id', (req, res) => {
-    db.get('SELECT rowid AS id, info FROM lorem WHERE rowid=?', req.params.id.toString(), (err, row) => {
+app.get('/items/:id', (req, res) => {
+    db.get('SELECT rowid AS id, name FROM items WHERE rowid=?', req.params.id.toString(), (err, row) => {
         if( err || !row ){
-            res.send( err || 404);
+            res.status(404).send( `This item was not found in the DataBase. <br /> ${ err }`);
         } else {
-            res.send({
-                id: row.id,
-                info: row.info
-            });
+            res.send( JSON.stringify( row ) );
         }
     });
 });
-
 
 /**
  * Start the Server
